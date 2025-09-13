fe3207a598753df5c0007353f28b82f4d4fcee4a
gurudeb9339 <senguptaargha17@gmail.com>
2025-09-13 13:18:11 +0530

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/footer/footer'
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Art from './components/banner/banner'
-=======
 import Cardlist from './components/cardlist/cardlist'
 
 
->>>>>>> 15995fc6afeca7be0d3f08cb3b75de94ba478ef7
-
-=======
-import Navbar from './components/footer/component/navbar/navbar'
-import CreateEvent from './components/create/createEvent'
-import Cardlist from './components/cardlist/cardlist'
-
-
->>>>>>> 00ba2ce8f7a192443fb1b0a2d4f891ea377b0315
 function App() {
   return (
     <>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <Art />
-      <h1>My Work</h1>
-      <Footer />
-=======
-    <Cardlist/>
-    <h1>My Work</h1>
-    <Footer/>
-=======
       <Navbar />
       <Cardlist />
       <CreateEvent />
       <Footer />
->>>>>>> 00ba2ce8f7a192443fb1b0a2d4f891ea377b0315
 
->>>>>>> 15995fc6afeca7be0d3f08cb3b75de94ba478ef7
     </>
   )
 }
