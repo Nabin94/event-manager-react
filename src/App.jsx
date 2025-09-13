ba8aeee328afc970ecd014e1177fb6d43cc998c8
Pankaj-web861 <pankajpramanik002@gmail.com>
2025-09-13 11:03:48 +0530

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/footer/footer'
+import Navbar from './components/footer/component/navbar/navbar'
 
 function App() {
 
 
   return (
     <>
-
+     <Navbar/>
     <h1>My Work</h1>
     <Footer/>
 
