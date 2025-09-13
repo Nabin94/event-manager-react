79a6f97b0498b729bd60ba1462fd38eccaab91d7
Nabin Kr Misra <Nabinkrmisra94@gmail.com>
2025-09-13 13:44:27 +0530

    Footer babai (#3)
    * requied folder added
    
    * css files  added

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './App.css'
 import Footer from './components/footer/footer'
+
+import HeroSection from './components/heroSection/heroSection'
 import Art from './components/banner/banner'
 import Navbar from './components/footer/component/navbar/navbar'
 import CreateEvent from './components/create/createEvent'
 import Cardlist from './components/cardlist/cardlist'
-import CreateEvent from './components/create/createEvent'
-import Navbar from './components/navbar/navbar'
+
 
 function App() {
   return (
     <>
+
+      
       <Navbar/>
       <Art/>
       <Navbar />
       <Cardlist />
+      <HeroSection/>
       <CreateEvent />
       <Footer />
-
     </>
   )
 }
