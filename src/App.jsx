f9f380c83fabf326e7371b2f8d16be85115b43fe
gurudeb9339 <senguptaargha17@gmail.com>
2025-09-13 13:28:03 +0530

    Added Banner (#2)
    * photo banner add
    
    * added banner
    
    * resolved merge conflict
    
    * added banner
    
    * added create event
    
    * added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Footer from './components/footer/footer'
+import Art from './components/banner/banner'
 import Navbar from './components/footer/component/navbar/navbar'
 import CreateEvent from './components/create/createEvent'
 import Cardlist from './components/cardlist/cardlist'
-
+import CreateEvent from './components/create/createEvent'
+import Navbar from './components/navbar/navbar'
 
 function App() {
-
-
   return (
     <>
+      <Navbar/>
+      <Art/>
       <Navbar />
       <Cardlist />
       <CreateEvent />
---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,11 @@
+import myImage from "./event_manager.png"
+
+export default function Art() {
+    return (
+        <div>
+            <section className="banner">
+                <img src={myImage}></img>
+            </section>
+        </div>
+    )
+}
