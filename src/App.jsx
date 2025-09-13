c82cd2edb35ed3a935ca21460e302bd35d1b59f9
gurudeb9339 <senguptaargha17@gmail.com>
2025-09-13 13:24:14 +0530

    added create event

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 import Footer from './components/footer/footer'
 import Art from './components/banner/banner'
 import Cardlist from './components/cardlist/cardlist'
+import CreateEvent from './components/create/createEvent'
 
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Banner/>
+      <Art/>
       <Cardlist />
       <CreateEvent />
       <Footer />
