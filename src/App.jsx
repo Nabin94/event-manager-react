78fd7038a68cc11c9fb4ac5dd08d0e1bbf3af7b4
gurudeb9339 <senguptaargha17@gmail.com>
2025-09-13 13:26:10 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Footer from './components/footer/footer'
 import Art from './components/banner/banner'
 import Cardlist from './components/cardlist/cardlist'
 import CreateEvent from './components/create/createEvent'
+import Navbar from './components/navbar/navbar'
 
 
 function App() {
   return (
     <>
+      <Navbar/>
       <Art/>
       <Cardlist />
       <CreateEvent />
