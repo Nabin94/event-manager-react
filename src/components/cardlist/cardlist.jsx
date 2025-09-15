e273bd9416b884722957f62303aa7973501eb667
DC <debayudh.maldaitbss@gmail.com>
2025-09-15 21:40:06 +0530

    cardlist data getting done

--- src/components/cardlist/cardlist.jsx
+++ src/components/cardlist/cardlist.jsx
@@ -1,23 +1,11 @@
 import './cardlist.scss'
+import LoadEvents from './cardlListFilter';
 
 function Cardlist(){
     return(
         <div className="container">
             <section className="card-list" id="card-list">
-                <div className="card">
-                    <div className="card-contents">
-                        <div className="card-icon">
-                            <i className="fa-solid fa-circle-info"></i>
-                        </div>
-                        <div className="card-section">
-                            <h1>Title</h1>
-                            <h2>15th August 2025</h2>
-                            <p>Independence Day Event </p>
-                            <p>@ Mathurapur BSS High School </p>
-                            <button className="card-action"> Read More</button>
-                        </div>
-                    </div>
-                </div>
+                <LoadEvents/>
             </section>
         </div>
     )   
