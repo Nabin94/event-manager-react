b4b89d427e2acf21692b52afb252fe908ed14c5e
Nabin94 <Nabinkrmisra94@gmail.com>
2025-09-18 22:10:07 +0530

    resolved merge conflict

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,9 @@ import CreateEvent from './components/create/createevent'
 import About from "./components/about/about"
 import HomePage from './components/homepage/home'
 import { Routes, Route } from 'react-router-dom'
-<<<<<<< HEAD
-import HeroSection from './components/heroSection/heroSection';
 import SearchEvent from './components/searchEvent/searchEvent';
 
 
-=======
->>>>>>> @{-1}
 function App() {
   return (
     <>
