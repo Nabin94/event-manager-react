5c966f240422d695863326886e73d88b7eb418e2
Pankaj Pramanik <pankajpramanik002@gmail.com>
2025-09-19 20:46:53 +0530

    Create/pankaj (#13)
    * updated react js in createevent page
    
    * added searchevent.jsx and searchevent.scss
    
    * updated files
    
    * updated about page

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css'
 import Footer from './components/footer/footer'
 import Navbar from './components/navbar/navbar'
 import CreateEvent from './components/create/createevent'
-import About from "./components/about/about"
+import About from './components/about/about'
 import HomePage from './components/homepage/home'
+import SearchEvent from './components/searchEvent/searchEvent'
 import { Routes, Route } from 'react-router-dom'
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<About />} />
         <Route path="/create" element={<CreateEvent />} />
+        <Route path="/searchEvent" element={<SearchEvent />} />
       </Routes>
       <Footer />
     </>
